2f0fd87a850e37cf80380ce3773f26a0d54c3b6d
Valerii <shklovskiyvalery@gmail.com>
2025-11-24 16:37:36 +0100

    :Configured docker-compose for db.

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator
+"""
+
+
+def add(x, y):
+    return x + y
+
+
+def substract(x, y):
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,15 @@
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        res = calc.substract(10, 15)
+
+        self.assertEqual(res, -5)
